83f0c6f318bcadaeee715847e8505be4b6c901e9
mostafaabdalmajeed70 <mostafaabdalmajeed70@gmail.com>
2026-08-09 07:08:26 +0300

    Wire routes and dependencies

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const connection = require('./db/connection.js')
+const userRoutes = require('./Routs/userRouts.js')
+const recipeRoutes = require('./Routs/recipeRouts.js')
 const port = 7777
 
 app.use(express.json())
+
+app.use('/users', userRoutes)
+app.use('/recipes', recipeRoutes)
+
 connection()
 
 app.listen(port, () => {
