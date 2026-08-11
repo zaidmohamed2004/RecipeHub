2ff3fda5dfd68a25479c8b6fa1f58cea476c69f2
Hoda Hatem <hodahatem324@gmail.com>
2026-08-11 19:44:47 +0300

    Added validation middleware and validation files, and created a config folder for database connection and JWT configuration.

--- Routs/userRouts.js
+++ Routs/userRouts.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/userController.js")
-const loginValidation = require("../middlewares/loginValidation.js");
-
-const registerValidation = require("../middlewares/registerValidation.js")
-
+const loginValidation = require("../validations/loginValidation.js");
+const resultValidation = require("../middlewares/resultValidation.js");
+const registerValidation = require("../validations/registerValidation.js")
 
 const router = express.Router();
 
-router.post("/register", registerValidation, userController.register);
 
-router.post("/login", loginValidation, userController.login);
+router.post("/register", registerValidation, resultValidation, userController.register);
+
+router.post("/login", loginValidation, resultValidation,userController.login);
+
 
 module.exports = router;
