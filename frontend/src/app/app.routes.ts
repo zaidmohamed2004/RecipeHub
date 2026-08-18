0bc1abd684d948fd339c4e6f11efde893e5324e4
Hoda Hatem <hodahatem324@gmail.com>
2026-08-18 13:52:15 +0300

    add footer component, login and register pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
   }
 ];
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
-export class Register {}
+export class Register {
+
+}
