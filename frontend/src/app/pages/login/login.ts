ac6d7563a69928432477e39e9ef63bcb6a048018
nada mostafa <nadam67455@gmail.com>
2026-08-19 09:11:24 +0300

    update register and login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  user = {
+    email: '',
+    password: ''
+  };
+
+  handleSubmit(form: NgForm) {
+
+    if (form.invalid) {
+      return;
+    }
+
+    console.log('Login data:', this.user);
+  }
 }
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  handleSubmit(form: NgForm) {
+
+    if (form.invalid) {
+      return;
+    }
+
+    if (this.user.password !== this.user.confirmPassword) {
+      return;
+    }
+
+    console.log('Form submitted:', this.user);
+  }
 }
